79c1fb61b71a47f98d22ecc3b9aa71a09363924c
Tom Molton <moltontom@Toms-Air.home>
2023-12-15 17:37:08 +0000

    15/12/2023: Need to sort delpoyment build issue

--- src/pages/Global/ControlCentre/ControlCentre.jsx
+++ src/pages/Global/ControlCentre/ControlCentre.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 import { Notifications } from "../../../components/Global/ControlCentre/Notifications/Notifications"
 import { ProfileCreator } from "../../../components/Global/ControlCentre/Selections/ProfileCreator.jsx/ProfileCreator"
 import './control-centre.styles.css'
-// import { ProfileIcon } from "../../../components/Global/Icons/Icons"
+import { ProfileIcon } from "../../../components/Global/Icons/Icons"
 
 export const ControlCentre = () => {
     return (
@@ -14,7 +14,7 @@ export const ControlCentre = () => {
                 <Notifications />
                 <div className='selections'>
                     <Link to={''} className='link selections-card'>
-                        {/* <ProfileIcon /> */}
+                        <ProfileIcon />
                         <h5 className='title'>Profile Creator</h5>
                         <p className='info'>Whether you're a musician, host or gig-goer, you can create your profile here.</p>
                     </Link>
