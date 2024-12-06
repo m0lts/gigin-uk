ac8272d0158b088eea5e28128aa7f1dcbb3d8de8
Tom Molton <moltontom@Toms-MacBook-Air.local>
2024-12-06 13:43:29 +0000

    Fixed venue side stripe issues

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { GigsProvider } from './context/GigsContext.jsx'
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 
-const stripePromise = loadStripe('pk_test_51Py8lOHI8M50kHhR49I0lIAR8gMId69DubgtmTEPQfHJV9JQSBVbflPSq0J8AT1kZUMqDHncMP0xdfvy3pGyQEOG002PN3x3dT');
+const stripePromise = loadStripe('pk_live_51Py8lOHI8M50kHhRvjzq6CjfiliJkeME3oaFpOhWLkIcx750xKeR68QwEgoanKf2GpH9ynizyl5RA1E1mBs5z9HQ00YNyhYnat');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
